129b50defc1b8baf5e17e0ffdf2f5bd68521b236
Anik Barua <anikbb05@gmail.com>
2025-06-18 20:09:01 +0600

    Add project files.

---
+++ Core_Assignment_2/Models/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Assignment_2.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        [StringLength(50, ErrorMessage = "Category cannot exceed 50 characters.")]
+        public required string Category { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a non-negative number.")]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative number.")]
+        public int Quantity { get; set; }
+    }
+}
+
